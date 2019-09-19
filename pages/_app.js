1b770979f61ddb1b9eed9dccf9f3c0919ad7037c
Ivan <ivanvlora@gmail.com>
2019-09-19 23:04:51 +0900

    added service worker to app

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import withReduxStore from '../lib/with-redux-store';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import OfflineSupport from '../components/OfflineSupport';
+import Head from 'next/head';
 
 const theme = {
   colors: {
@@ -18,7 +19,36 @@ class MyApp extends App {
     const { Component, pageProps, reduxStore } = this.props;
     return (
       <Provider store={reduxStore}>
-        <OfflineSupport />
+        <Head>
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/apple-touch-icon.png"
+          />
+          >
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicon-16x16.png"
+          />
+          <link
+            rel="mask-icon"
+            href="/static/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="theme-color" content="#ffffff" />{' '}
+          <link rel="manifest" href="/static/manifest.json" />
+          <link rel="manifest" href="/static/site.webmanifest" />
+        </Head>
         <ThemeProvider theme={theme}>
           <Component {...pageProps} />
         </ThemeProvider>
